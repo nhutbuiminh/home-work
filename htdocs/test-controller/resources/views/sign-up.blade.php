d1847cbc106ca4773aaf0ac7735685e4bd110710
vipbbmnvip <nhutbui2903@gmail.com>
2019-01-19 16:33:43 +0700

    update ss1

---
+++ htdocs/test-controller/resources/views/sign-up.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <form action="/sign-up.blade.php">
+        <label for="">Email address</label>
+        <input type="email" placeholder="Enter your email">
+        <label for="">Email address</label>
+        <input type="password" placeholder="Enter your password">
+    </form>
+</body>
+</html>
